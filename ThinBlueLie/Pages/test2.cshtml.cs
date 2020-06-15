be91a8450bb86df473d0583769c43140a9eeb879
Bubinga <eli.h.bell@gmail.com>
2020-06-15 03:09:42 -0600

    week change, stole somebody's month format

---
+++ ThinBlueLie/Pages/test2.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ThinBlueLie.Pages
+{
+    public class test2Model : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
