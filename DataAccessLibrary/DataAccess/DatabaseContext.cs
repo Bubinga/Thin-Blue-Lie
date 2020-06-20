e4fb3e48452418a30ece794f85eb3a8d2fbe5fc3
Bubinga <eli.h.bell@gmail.com>
2020-06-19 21:56:13 -0600

    Set up class library and got closer to reading from database

---
+++ DataAccessLibrary/DataAccess/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using DataAccessLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLibrary.DataAccess
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options) { }
+        public DbSet<Test> Test { get; set; }
+    }
+}
--- ThinBlueLie/Pages/test.cshtml.cs
+++ ThinBlueLie/Pages/test.cshtml.cs
@@ -4,14 +4,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Web;
+
 
 namespace ThinBlueLie.Pages
-{
+{    
     public class testModel : PageModel
     {
         public void OnGet()
         {
+            BindData();
+        }
 
+        public void BindData()
+        {
+           
         }
     }
 }
