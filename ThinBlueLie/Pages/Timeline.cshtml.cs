40f6dfd68679d519f4628c4cdb6ea663c339dc4f
Bubinga <eli.h.bell@gmail.com>
2020-06-20 21:59:09 -0600

    New migration, created new models and context for MySql server which now has the proper table in it. Removed TimelineLayout and put its contents into Timeline. Moved month javascript to site.js, made format buttons color change on press.

--- ThinBlueLie/Pages/Timeline.cshtml.cs
+++ ThinBlueLie/Pages/Timeline.cshtml.cs
@@ -4,14 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using DataAccessLibrary.thinblue;
+using MySql.Data.MySqlClient;
+using DataAccessLibrary;
+
 
 namespace ThinBlueLie.Pages
 {
     public class TimelineModel : PageModel
     {
+        [BindProperty(SupportsGet = true)]
+        public string Date { get; set; }       
+        
         public void OnGet()
         {
-
+            if (string.IsNullOrWhiteSpace(Date))
+            {
+                Date = DateTime.Now.ToString("d");
+            }
         }
     }
 }
