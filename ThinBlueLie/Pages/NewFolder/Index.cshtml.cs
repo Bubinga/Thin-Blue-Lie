b528a0239c58575a152634bbb78cf051dd32d5c2
Bubing-a <Bubing-a@users.noreply.github.com>
2020-06-21 02:16:41 -0600

    Finally got CRUD working through Html.

---
+++ ThinBlueLie/Pages/NewFolder/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DataAccessLibrary.thinblue;
+
+namespace ThinBlueLie.Pages.NewFolder
+{
+    public class IndexModel : PageModel
+    {
+        private readonly DataAccessLibrary.thinblue.ThinbluelieContext _context;
+
+        public IndexModel(DataAccessLibrary.thinblue.ThinbluelieContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Timelineinfo> Timelineinfo { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Timelineinfo = await _context.Timelineinfo.ToListAsync();
+        }
+    }
+}
