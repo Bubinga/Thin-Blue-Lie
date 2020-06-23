388c33ac385a051e32959c0f8dcb3ffbd8851869
Bubinga <eli.h.bell@gmail.com>
2020-06-23 04:09:54 -0600

    Added Submit Info page, Login, Register and added Users table from database

---
+++ DataAccessLibrary/Models/Users.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLibrary.thinblue
+{
+    public partial class Users
+    {
+        public int IdUsers { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
