16815326626dc76dbaf69c134f6314273e5e724a
Bubinga <eli.h.bell@gmail.com>
2020-06-24 02:19:32 -0600

    Added Sign-in w/ Google. haven't figured out how to draw profile info from it though

--- ThinBlueLie/wwwroot/js/site.js
+++ ThinBlueLie/wwwroot/js/site.js
@@ -2,6 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+}
+
+
+
+
 var Calendar = function (o) {
     //Store div id
     this.divId = o.ParentID;
