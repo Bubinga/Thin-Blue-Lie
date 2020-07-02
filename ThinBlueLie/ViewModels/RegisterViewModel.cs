c5cf508abca1dfd18c4ffbc067777734be659ce1
Bubinga <eli.h.bell@gmail.com>
2020-07-02 02:46:45 -0600

    Added Viewmodel folder with RegisterViewModel. Got Identity registration working successfully.

---
+++ ThinBlueLie/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThinBlueLie.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+    }
+}
