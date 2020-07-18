c396d517efc9fd059555b8dfa482bf867f867837
Bubing-a <Bubing-a@users.noreply.github.com>
2020-07-17 23:24:48 -0600

    In Submit Media, Choose File, and Link Media appear and show on respective Source selects

--- ThinBlueLie/Pages/test3.cshtml.cs
+++ ThinBlueLie/Pages/test3.cshtml.cs
@@ -6,21 +6,13 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using ThinBlue;
 
 namespace ThinBlueLie.Pages
 {
     public class Test3Model
     {
-       [Required]
-       [EmailAddress]
-        public string Email { get; set; }
+        public Media Medias { get; set; }
 
-       [Required]
-       [DataType(DataType.Password)]
-       public string Password { get; set; }
-
-       [Display(Name = "Remember me")]
-       public bool RememberMe { get; set; }
-        
     }
 }
