f7931ede62483f40537035c02492bc3887e6e92d
Bubinga <eli.h.bell@gmail.com>
2020-07-21 03:39:43 -0600

    Add Log in code and database

---
+++ ThinBlueLie/Models/Log.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThinBlue
+{
+    public class Log
+    {
+        [Key]
+        public int IdLog { get; set; }
+        public string TimeStamp { get; set; }
+        public int Action { get; set; }
+        public string IpAddress { get; set; }
+        public string IdUser { get; set; }
+        public int IdTimelineinfo { get; set; }
+    }
+}
