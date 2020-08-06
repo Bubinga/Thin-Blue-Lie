8592e91a696eb0ee8db5a9f85bad3cb39cda3347
Bubinga <eli.h.bell@gmail.com>
2020-08-05 23:09:14 -0600

    Moved Enums into separate folder, and added junction model classes

---
+++ ThinBlueLie/Models/Junction/Junc_Timelineinfo_Officer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThinBlue
+{
+    public class LinkTimelineinfo_Officer
+    {
+        [Key]
+        public int IdLinkTimelineinfo_Officer { get; set; }
+        public int IdTimelineinfo { get; set; }
+        public int IdOfficer { get; set; }
+    }
+}
