d24b2d8dd16bfafad54e5f2703d2d2436435b181
Bubinga <eli.h.bell@gmail.com>
2020-08-15 05:03:55 -0600

    Added SimilarPersonPartial

--- DataAccessLibrary/Enums/TimelineinfoEnums.cs
+++ DataAccessLibrary/Enums/TimelineinfoEnums.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
 
 namespace DataAccessLibrary.Enums
 {
---
+++ ThinBlueLie/ViewModels/Person.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace ThinBlueLie.ViewModels
+{
+    public class SimpleTimelineinfo
+    {
+        public string Date { get; set; }
+        public string City { get; set; }
+        public int State { get; set; }
+    }
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Race { get; set; }
+        public int Sex { get; set; }
+        public List<SimpleTimelineinfo> Events { get; set; }
+    }
+}
