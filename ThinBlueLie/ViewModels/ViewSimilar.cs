3b87f9fe96b5c1e2fb0a47979a0b85bb5c4270e1
Bubinga <eli.h.bell@gmail.com>
2020-08-18 05:09:38 -0600

    Fixed HasnoKey error

--- ThinBlueLie/ViewModels/ViewSimilar.cs
+++ ThinBlueLie/ViewModels/ViewSimilar.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace ThinBlueLie.ViewModels
 {
     public class ViewSimilar
-    {
+    {       
         public Timelineinfo Timelineinfo { get; set; }
         public List<string> Officers { get; set; }
         public List<string> Subjects { get; set; }
