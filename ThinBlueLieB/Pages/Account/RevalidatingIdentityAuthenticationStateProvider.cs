a888de7993ccc68f929c3c0e04826f5b52fc62ad
Bubinga <eli.h.bell@gmail.com>
2020-08-20 02:57:09 -0600

    Added navbar and icon.

--- ThinBlueLieB/Pages/Account/RevalidatingIdentityAuthenticationStateProvider.cs
+++ ThinBlueLieB/Pages/Account/RevalidatingIdentityAuthenticationStateProvider.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace ThinBlueLieB.Areas.Identity
+namespace ThinBlueLieB.Identity
 {
     public class RevalidatingIdentityAuthenticationStateProvider<TUser>
         : RevalidatingServerAuthenticationStateProvider where TUser : class
