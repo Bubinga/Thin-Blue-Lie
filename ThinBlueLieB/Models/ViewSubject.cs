0b463b8b535f5ca53d99f1741ddd6313c686d032
Bubinga <eli.h.bell@gmail.com>
2020-08-21 23:56:35 -0600

    Added Flag, Media, Officer, Subject, and Test Components along with their respective classes

---
+++ ThinBlueLieB/Models/ViewSubject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThinBlueLieB.Models
+{
+    public class ViewSubject
+    {
+        public int IdSubject { get; set; }
+        public string Name { get; set; }
+        [Required(ErrorMessage = "The Subject's Race field is required")]
+        public byte Race { get; set; }
+        [Required(ErrorMessage = "The Subject's Sex field is required")]
+        public byte Sex { get; set; }
+        public bool SameAs { get; set; }
+        public bool Armed { get; set; }
+    }
+}
