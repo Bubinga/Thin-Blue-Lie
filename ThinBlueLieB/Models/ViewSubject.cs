85ed01cd1b686c94a351b039652fe1e795f27dcb
Bubing-a <Bubing-a@users.noreply.github.com>
2020-09-03 20:35:10 -0600

    Added add and delete to Media, Subject, and Officer Lists. Also added move-up, move-down, move-top to Media.

--- ThinBlueLieB/Models/ViewSubject.cs
+++ ThinBlueLieB/Models/ViewSubject.cs
@@ -5,6 +5,7 @@ namespace ThinBlueLieB.Models
     public class ViewSubject
     {
         public int IdSubject { get; set; }
+        public int ListIndex { get; set; }
         public string Name { get; set; }
         [Required(ErrorMessage = "The Subject's Race field is required")]
         public byte Race { get; set; }
