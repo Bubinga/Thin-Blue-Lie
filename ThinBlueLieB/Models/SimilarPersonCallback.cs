80445c2ac8425989d61be87a420c4c4bc8bece7c
Bubinga <eli.h.bell@gmail.com>
2020-09-08 01:35:32 -0600

    Added support for multiple different similar people and proper handling of similar people upon person delete.

---
+++ ThinBlueLieB/Models/SimilarPersonCallback.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThinBlueLieB.Models
+{
+    public class SimilarPersonCallback
+    {
+        public string Name { get; set; }
+        public int Index { get; set; }
+    }
+}
