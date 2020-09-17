53b46b3db902e9be6751c73ff8ea1e9f7db06369
Bubinga <eli.h.bell@gmail.com>
2020-09-16 19:03:35 -0600

    Added Regex expressions in Submit, fixed add media/subject button formatting, and added validation to components

--- ThinBlueLieB/Models/ViewSubject.cs
+++ ThinBlueLieB/Models/ViewSubject.cs
@@ -6,11 +6,14 @@ namespace ThinBlueLieB.Models
     {
         public int IdSubject { get; set; }
         public int ListIndex { get; set; }
+        [RegularExpression("(?i)^(?:(?![×Þß÷þø])[-'a-zÀ-ÿ ])+$", ErrorMessage = "Enter only letters")]
         public string Name { get; set; }
         [Required(ErrorMessage = "The Subject's Race field is required")]
         public byte Race { get; set; }
         [Required(ErrorMessage = "The Subject's Sex field is required")]
         public byte Sex { get; set; }
+        [Range(0, 130,
+        ErrorMessage = "Age must be between {1} and {2}.")]
         public int? Age { get; set; }
         public bool SameAs { get; set; }
         public bool Armed { get; set; }
