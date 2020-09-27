5679a1953627ab9a8fb61e2614dadfb67725e232
Bubing-a <Bubing-a@users.noreply.github.com>
2020-09-26 22:30:14 -0600

    Scaffolded all Identity pages for later reference

---
+++ ThinBlueLieB/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ThinBlueLieB.Data;
+
+[assembly: HostingStartup(typeof(ThinBlueLieB.Areas.Identity.IdentityHostingStartup))]
+namespace ThinBlueLieB.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
