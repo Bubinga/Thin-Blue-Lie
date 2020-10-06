652bf89927244321d946410c3096ba4bcaf86014
Bubinga <eli.h.bell@gmail.com>
2020-10-05 22:11:23 -0600

    Setup Youtube API and got working a simple embed video

---
+++ ThinBlueLieB/wwwroot/js/YoutubeAPI.js
@@ -0,0 +1,36 @@
+﻿// 2. This code loads the IFrame Player API code asynchronously.
+var id;
+function loadYTScript(input) {
+    id = input;
+    var tag = document.createElement('script');
+
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+}
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '390',
+        width: '640',
+        videoId: id,
+        playerVars: { 'rel': 0}
+    });
+}
+
+function LoadVideo() {
+    loadYTScript();
+    onYouTubeIframeAPIReady();
+}
+
+function stopVideo() {
+    player.stopVideo();
+}
+
+function stopVideo() {
+    player.playVideo();
+}
