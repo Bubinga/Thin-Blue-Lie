1c580013551adcac20decffb489ee7e7377120da
Bubinga <eli.h.bell@gmail.com>
2020-10-06 23:28:40 -0600

    Added YoutubeComponent and an empty MediaCarousel Component

--- ThinBlueLieB/wwwroot/js/YoutubeAPI.js
+++ ThinBlueLieB/wwwroot/js/YoutubeAPI.js
@@ -1,24 +1,28 @@
 ﻿// 2. This code loads the IFrame Player API code asynchronously.
-var id;
-function loadYTScript(input) {
-    id = input;
-    var tag = document.createElement('script');
+////var id;
+//    var tag = document.createElement('script');
 
-    tag.src = "https://www.youtube.com/iframe_api";
-    var firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-}
+//    tag.src = "https://www.youtube.com/iframe_api";
+//    var firstScriptTag = document.getElementsByTagName('script')[0];
+//    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 // 3. This function creates an <iframe> (and YouTube player)
 //    after the API code downloads.
 
-var player;
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-        height: '390',
-        width: '640',
-        videoId: id,
-        playerVars: { 'rel': 0}
+//var player;
+//function onYouTubeIframeAPIReady() {
+//    player = new YT.Player('player', {
+//        height: '390',
+//        width: '640',
+//        videoId: id,
+//        playerVars: { 'rel': 0}
+//    });
+//}
+
+function loadYouTubeFromId(inputId) {
+    player = new YT.Player(inputId, {      
+        videoId: inputId,
+        playerVars: { rel: 0, showinfo: 0, ecver: 2 }
     });
 }
 
