9174d44f535bd81feb321a27cb9212515a0ca224
Bubinga <eli.h.bell@gmail.com>
2020-10-08 23:49:54 -0600

    Added functional but ugly carousel w/ video support

---
+++ ThinBlueLieB/wwwroot/js/Carousel.js
@@ -0,0 +1,17 @@
+﻿var galleryThumbs = new Swiper(".gallery-thumbs", {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+});
+var galleryTop = new Swiper(".gallery-top", {
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+        swiper: galleryThumbs,
+    },
+});
