c1d95022816220a5e18323008199bfbb68a96f5e
Bubinga <eli.h.bell@gmail.com>
2020-10-22 15:24:11 -0600

    Moved Account Models and View Models into folders

---
+++ ThinBlueLieB/Models/Account Models/ProfileModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThinBlueLieB.Models
+{
+    public class ProfileModel
+    {
+    }
+}
