db60d64dbca8502148ae4c04fadbeb6c2a74d02b
Bubinga <eli.h.bell@gmail.com>
2020-10-21 22:18:33 -0600

    Fixed carousel formatting and added swiperjs activation on events page.

--- ThinBlueLieB/wwwroot/js/Carousel.js
+++ ThinBlueLieB/wwwroot/js/Carousel.js
@@ -1,17 +1,40 @@
-﻿var galleryThumbs = new Swiper(".gallery-thumbs", {
-    spaceBetween: 10,
-    slidesPerView: 4,
-    freeMode: true,
-    watchSlidesVisibility: true,
-    watchSlidesProgress: true,
-});
-var galleryTop = new Swiper(".gallery-top", {
-    spaceBetween: 10,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    thumbs: {
-        swiper: galleryThumbs,
-    },
-});
+﻿var galleryThumbs;
+var galleryTop;
+function InitializeSwiper() {
+    galleryThumbs = new Swiper(".gallery-thumbs", {
+        spaceBetween: 2,
+        slidesPerView: 'auto',
+        loop: true,
+        loopedSlides: $(".swiper-wrapper").length,
+        freeMode: true,
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true
+        //breakpoints: {
+        //    640: {
+        //        slidesPerView: 2,
+        //        spaceBetween: 20,
+        //    },
+        //    768: {
+        //        slidesPerView: 4,
+        //        spaceBetween: 40,
+        //    },
+        //    1024: {
+        //        slidesPerView: 5,
+        //        spaceBetween: 50,
+        //    }
+        //}
+    });
+    galleryTop = new Swiper(".gallery-top", {
+        spaceBetween: 10,
+        slidesPerView: 'auto',
+        loop: true,
+        loopedSlides: $(".swiper-wrapper").length,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        thumbs: {
+            swiper: galleryThumbs,
+        },
+    });
+}
