b2df21c0b998e47e6c0db8694b04a579e0759643
Bubinga <eli.h.bell@gmail.com>
2020-10-24 18:53:50 -0600

    Added edit.razor and put the methods that it and submit.razor share in an inherited class

---
+++ ThinBlueLieB/Bases/EditBase.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ThinBlueLieB.Models;
+using static ThinBlueLieB.Models.SubmitBase;
+
+namespace ThinBlueLieB.Bases
+{
+    public class EditBase : InformationBase
+    {
+        public new SubmitModel model = new SubmitModel()
+        {
+            Timelineinfos = new ViewTimelineinfo(),
+            Medias = new List<ViewMedia> { new ViewMedia {} },
+            Officers = new List<ViewOfficer> { new ViewOfficer { } },
+            Subjects = new List<ViewSubject> { new ViewSubject { } }
+        };
+        protected override Task OnInitializedAsync()
+        {
+            
+        }
+        //fill out timelineinfo
+        //fill out media
+        //fill out officers
+        //fill out subjects
+
+    }
+}
--- ThinBlueLieB/Bases/SubmitBase.cs
+++ ThinBlueLieB/Bases/SubmitBase.cs
@@ -1,54 +1,14 @@
-﻿using DataAccessLibrary.DataModels;
-using DataAccessLibrary.Enums;
-using Microsoft.AspNetCore.Components;
+﻿using DataAccessLibrary.Enums;
 using Syncfusion.Blazor.RichTextEditor;
-using Syncfusion.Licensing;
-using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-using ThinBlueLieB.Helper;
+using ThinBlueLieB.Bases;
 using ThinBlueLieB.Helper.Extensions;
-using static ThinBlueLieB.Searches.SearchClasses;
 
 namespace ThinBlueLieB.Models
 {
-    public partial class SubmitBase : ComponentBase
+    public partial class SubmitBase : InformationBase
     {
-        public SubmitModel model = new SubmitModel()
-        {
-            Timelineinfos = new ViewTimelineinfo(),
-            Medias = new List<ViewMedia> { new ViewMedia { ListIndex = 0 } },
-            Officers = new List<ViewOfficer> { new ViewOfficer { ListIndex = 0 } },
-            Subjects = new List<ViewSubject> { new ViewSubject { ListIndex = 0 } }
-        };
-
-        public List<ToolbarItemModel> Tools = new List<ToolbarItemModel>()
-        {
-            new ToolbarItemModel() { Command = ToolbarCommand.Bold },
-            new ToolbarItemModel() { Command = ToolbarCommand.Italic },
-            new ToolbarItemModel() { Command = ToolbarCommand.Underline },
-            new ToolbarItemModel() { Command = ToolbarCommand.Separator },
-            new ToolbarItemModel() { Command = ToolbarCommand.Formats },
-            new ToolbarItemModel() { Command = ToolbarCommand.Alignments },
-            new ToolbarItemModel() { Command = ToolbarCommand.OrderedList },
-            new ToolbarItemModel() { Command = ToolbarCommand.UnorderedList },
-            new ToolbarItemModel() { Command = ToolbarCommand.Outdent },
-            new ToolbarItemModel() { Command = ToolbarCommand.Indent },
-            new ToolbarItemModel() { Command = ToolbarCommand.Separator },
-            new ToolbarItemModel() { Command = ToolbarCommand.CreateLink },
-            new ToolbarItemModel() { Command = ToolbarCommand.CreateTable },
-         // new ToolbarItemModel() { Command = ToolbarCommand.Image }, TODO add support for context inline images
-            new ToolbarItemModel() { Command = ToolbarCommand.Separator },
-            new ToolbarItemModel() { Command = ToolbarCommand.ClearFormat },
-            new ToolbarItemModel() { Command = ToolbarCommand.SourceCode },
-            new ToolbarItemModel() { Command = ToolbarCommand.Separator },
-            new ToolbarItemModel() { Command = ToolbarCommand.Undo },
-            new ToolbarItemModel() { Command = ToolbarCommand.Redo }
-         };
-
-        public IEnumerable<string> States = EnumExtensions.GetEnumDisplayNames<TimelineinfoEnums.StateEnum>();
+       
 
         //add login suggestion
 
