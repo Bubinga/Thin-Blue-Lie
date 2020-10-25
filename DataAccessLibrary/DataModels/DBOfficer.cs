2946aa6f084fc244490c139be19c2433ea7fb8d7
Bubing-a <Bubing-a@users.noreply.github.com>
2020-10-25 14:33:04 -0600

    Removed Base and Display Officer/Subject and replaced them with DBOfficer/DBSubject

--- DataAccessLibrary/DataModels/DBOfficer.cs
+++ DataAccessLibrary/DataModels/DBOfficer.cs
@@ -1,16 +1,16 @@
 ﻿using DataAccessLibrary.Enums;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Text;
 
-namespace ThinBlueLieB.ViewModels
+namespace DataAccessLibrary.DataModels
 {
-    public class DisplayOfficer
+    public class DBOfficer
     {
+        public int IdOfficer { get; set; }
         public string? Name { get; set; }
-        public TimelineinfoEnums.RaceEnum Race { get; set; }
-        public TimelineinfoEnums.SexEnum Sex { get; set; }
+        public TimelineinfoEnums.RaceEnum? Race { get; set; }
+        public TimelineinfoEnums.SexEnum? Sex { get; set; }
         public int? Age { get; set; }
         public int Misconduct { get; set; }
         public int? Weapon { get; set; }
---
+++ DataAccessLibrary/DataModels/DBSubject.cs
@@ -0,0 +1,17 @@
+﻿using DataAccessLibrary.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLibrary.DataModels
+{
+    public class DBSubject
+    {
+        public int IdSubject { get; set; }
+        public string? Name { get; set; }
+        public TimelineinfoEnums.RaceEnum? Race { get; set; }
+        public TimelineinfoEnums.SexEnum? Sex { get; set; }
+        public int? Age { get; set; }
+        public bool Armed { get; set; }
+    }
+}
