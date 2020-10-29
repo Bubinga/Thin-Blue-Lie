d74df73c83514c2ca6842cec5c2a480e6e82ad50
Bubinga <eli.h.bell@gmail.com>
2020-10-28 20:30:10 -0600

    Extended IdentityUser to ApplicationUser

--- DataAccessLibrary/DataAccess/UserDbContext.cs
+++ DataAccessLibrary/DataAccess/UserDbContext.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using DataAccessLibrary.UserModels;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ThinBlueLieB.Data
 {
-    public class UserDbContext : IdentityDbContext
+    public class UserDbContext : IdentityDbContext<ApplicationUser>
     {
         public UserDbContext(DbContextOptions<UserDbContext> options)
             : base(options)
         {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
         }
     }
 }
---
+++ DataAccessLibrary/UserModels/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary.UserModels
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public DateTime DateJoined { get; set; }
+        public int Reputation { get; set; }
+    }
+}
