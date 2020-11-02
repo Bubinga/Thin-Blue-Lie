6784e034beb4fb17aca0866a73e3a45af14a7e41
Bubing-a <Bubing-a@users.noreply.github.com>
2020-11-01 23:17:58 -0700

    Updated packages and outlined edit review querying

---
+++ ThinBlueLieB/Models/View Models/EditReviewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ThinBlueLieB.ViewModels;
+
+namespace ThinBlueLieB.Models.View_Models
+{
+    public class EditReviewModel
+    {
+        public List<ViewEvent> Versions { get; set; }
+    }
+}
