2bc4058e95f4f81ed2567c32fadc5ec5bbcae294
Bubinga <eli.h.bell@gmail.com>
2020-11-03 17:35:21 -0700

    Added roles and edithistory

---
+++ DataAccessLibrary/DataModels/EditModels/EditHistory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLibrary.DataModels.EditModels
+{
+    class EditHistory
+    {
+        public int IdEditHistory { get; set; }
+        public byte Confirmed { get; set; }
+        public byte IdTimelineinfo { get; set; }
+        public DateTime Timestamp { get; set; }
+        public byte Edits { get; set; }
+        public byte EditMedia { get; set; }
+        public byte Officers { get; set; }
+        public byte Subjects { get; set; }
+        public byte Timelineinfo_Subject { get; set; }
+        public byte Timelineinfo_Officer { get; set; }
+    }
+}
