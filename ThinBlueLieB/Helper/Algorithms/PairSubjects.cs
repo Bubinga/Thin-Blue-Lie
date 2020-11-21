9454c0f834caa1c915c57843d95132ea8d3d654f
Bubing-a <Bubing-a@users.noreply.github.com>
2020-11-20 22:39:41 -0700

    Reformatted EditBase to work with new edit history system. WIP

---
+++ ThinBlueLieB/Helper/Algorithms/PairSubjects.cs
@@ -0,0 +1,58 @@
+﻿using DataAccessLibrary.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThinBlueLieB.Helper.Algorithms
+{
+    public partial class PairVersions
+    {
+        public List<Tuple<DBSubject, DBSubject>> PairSubjects(List<DBSubject> OldSubjects, List<DBSubject> NewSubjects)
+        {
+            List<Tuple<DBSubject, DBSubject>> Subjects = new List<Tuple<DBSubject, DBSubject>>();
+            List<int> pairedIds = new List<int>();
+            foreach (var subject in NewSubjects.Union(OldSubjects))
+            {
+                if (!pairedIds.Contains(subject.IdSubject))
+                {
+                    List<DBSubject> pair = NewSubjects.Union(OldSubjects).Where(o => o.IdSubject == subject.IdSubject).ToList();
+                    bool isPaired = false;
+                    Tuple<DBSubject, DBSubject> pairing;
+                    for (int i = 0; i < pair.Count; i++)
+                    {
+                        if (pair[i] != subject)
+                        {
+                            if (NewSubjects.Contains(pair[i]))
+                            {
+                                pairing = new Tuple<DBSubject, DBSubject>(subject, pair[i]);
+                            }
+                            else //if oldOfficer contains it
+                            {
+                                pairing = new Tuple<DBSubject, DBSubject>(pair[i], subject);
+                            }
+                            Subjects.Add(pairing);
+                            isPaired = true;
+                            pairedIds.AddRange(new int[] { subject.IdSubject, pair[i].IdSubject });
+                        }
+                    }
+                    if (isPaired == false)
+                    {
+                        if (OldSubjects.Contains(subject))
+                        {
+                            pairing = new Tuple<DBSubject?, DBSubject?>(null, subject);
+                        }
+                        else //if oldOfficer contains it
+                        {
+                            pairing = new Tuple<DBSubject?, DBSubject?>(subject, null);
+                        }
+                        Subjects.Add(pairing);
+                        pairedIds.Add(subject.IdSubject);
+                    }
+                }
+            }
+            return Subjects;
+        }
+
+    }
+}
---
+++ ThinBlueLieB/Helper/Extensions/ClassExtensions.cs
@@ -0,0 +1,36 @@
+﻿using DataAccessLibrary.DataModels;
+using DataAccessLibrary.DataModels.EditModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ThinBlueLieB.Models;
+
+namespace ThinBlueLieB.Helper.Extensions
+{
+    public static class ClassExtensions
+    {
+        public static bool ContainsChange(this EditHistory editHistory)
+        {
+            if (editHistory.Edits != 0 ||
+                editHistory.EditMedia != 0 ||
+                editHistory.Timelineinfo_Subject != 0 ||
+                editHistory.Timelineinfo_Officer != 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool JunctionChange<T>(this T subject, T oldSubject) where T : CommonPerson
+        {
+            if ((subject?.Name != oldSubject?.Name) || (subject?.Race != oldSubject?.Race) ||
+                        (subject?.Sex != oldSubject?.Sex) || (subject?.Image != oldSubject?.Image) ||
+                        (subject?.Local != oldSubject?.Local))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
