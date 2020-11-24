f438c36b3fd3fecfb260ae4111d7f99b76f63246
Bubing-a <Bubing-a@users.noreply.github.com>
2020-11-24 00:04:42 -0700

    Filled out Profile

---
+++ ThinBlueLieB/Helper/Extensions/IdentityExtensions.cs
@@ -0,0 +1,44 @@
+﻿using DataAccessLibrary.UserModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static DataAccessLibrary.Enums.PrivledgeEnum;
+
+namespace ThinBlueLieB.Helper.Extensions
+{
+    public static class IdentityExtensions
+    {
+        public static bool RepAuthorizer(this ApplicationUser User, Privledges Privledge)
+        {
+            if (User.Reputation >= (int)Privledge)
+                return true;
+            else
+                return false;
+        }
+        public static Privledges NextPrivledge(this ApplicationUser User)
+        {
+            int Rep = User.Reputation;
+            foreach (int privledge in Enum.GetValues(typeof(Privledges)))
+            {
+                if (Rep < privledge)
+                {
+                    return (Privledges)privledge;
+                }
+            }
+            return Privledges.CreateEditFlag;
+        }
+        public static Privledges CurrentPrivledge(this ApplicationUser User)
+        {
+            int Rep = User.Reputation;
+            foreach (int privledge in ((Privledges[])Enum.GetValues(typeof(Privledges))).OrderByDescending(x => x))
+            {
+                if (Rep > privledge)
+                {
+                    return (Privledges)privledge;
+                }
+            }
+            return Privledges.CreateEditFlag;            
+        }
+    }
+}
