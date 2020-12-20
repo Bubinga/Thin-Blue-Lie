488e4ad9df07f9021dd165273a39d835829a8082
Bubing-a <Bubing-a@users.noreply.github.com>
2020-12-19 18:20:14 -0700

    Put different events on the same day in differnt urls and event is directory based, not query strings now

---
+++ ThinBlueLie/Models/View Models/FirstLoadEvents.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThinBlueLie.Models.ViewModels
+{
+    public class FirstLoadEvents
+    {
+        public int IdTimelineinfo { get; set; }
+        public string Title { get; set; }
+        public string Context { get; set; }
+        public ViewMedia Media { get; set; }
+    }
+}
