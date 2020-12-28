22b11a2d84eea0f5524aa417de3abba7b28e88a6
Bubinga <eli.h.bell@gmail.com>
2020-12-28 01:03:32 -0700

    Cleaned up Event's OnParametersSetAsync logic

---
+++ ThinBlueLie/Helper/SqlHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThinBlueLie.Helper
+{
+    public static class SqlHelper
+    {
+        public static readonly string[] StopWords = {
+            "a",
+            "about",
+            "an",
+            "are",
+            "as",
+            "at",
+            "be",
+            "by",
+            "com",
+            "de",
+            "en",
+            "for",
+            "from",
+            "how",
+            "i",
+            "in",
+            "is",
+            "it",
+            "la",
+            "of",
+            "on",
+            "or",
+            "that",
+            "the",
+            "this",
+            "to",
+            "was",
+            "what",
+            "when",
+            "where",
+            "who",
+            "will",
+            "with",
+            "und",
+            "the",
+            "www"
+        };
+    }
+}
--- ThinBlueLie/Models/View Models/FirstLoadEvents.cs
+++ ThinBlueLie/Models/View Models/FirstLoadEvents.cs
@@ -8,6 +8,7 @@ namespace ThinBlueLie.Models.ViewModels
     public class FirstLoadEvents
     {
         public int IdTimelineinfo { get; set; }
+        public DateTime Date { get; set; }
         public string Title { get; set; }
         public string Context { get; set; }
         public ViewMedia Media { get; set; }
