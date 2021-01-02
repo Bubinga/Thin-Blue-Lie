78f0e0f470abab361bf298fb8d01330a18af1380
Bubinga <eli.h.bell@gmail.com>
2021-01-01 23:41:02 -0700

    Display what events are on a given day on hover

---
+++ ThinBlueLie/Helper/Algorithms/SeverityCalc.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThinBlueLie.Helper.Algorithms
+{
+    public class SeverityCalc
+    {
+    }
+}
