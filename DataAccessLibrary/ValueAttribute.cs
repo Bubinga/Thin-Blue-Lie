8305cf05b85d7134de13ba7a7eb8c86c38b71bfa
Bubinga <eli.h.bell@gmail.com>
2021-01-03 00:14:14 -0700

    Added Severity Calc and colors to timeline popup

---
+++ DataAccessLibrary/ValueAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary.Helper
+{
+    public class ValueAttribute : ValidationAttribute
+    {
+        public ValueAttribute(int value)
+        {
+            Value = value;
+        }
+
+        public int Value { get; }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            return ValidationResult.Success;
+        }
+    }
+}
+
---
+++ ThinBlueLie/Models/ViewModels/TimelineinfoFull.cs
@@ -0,0 +1,20 @@
+﻿using DataAccessLibrary.DataModels;
+using DataAccessLibrary.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThinBlueLie.Models.ViewModels
+{
+    public class TimelineinfoFull
+    {
+        public Timelineinfo Timelineinfo { get; set; }
+        public List<TimelineinfoOfficerShort> OfficerInfo { get; set; }
+        public class TimelineinfoOfficerShort
+        {
+            public TimelineinfoEnums.MisconductEnum Misconduct { get; set; }
+            public TimelineinfoEnums.WeaponEnum Weapon { get; set; }
+        }
+    }
+}
