eff9ed1d73d26daf517b29118cb6ed8fdfb0e2bd
Bubinga <eli.h.bell@gmail.com>
2021-01-12 19:32:45 -0700

    Changed all db table name references to lowercase

--- DataAccessLibrary/UserModels/Aspnetroleclaims.cs
+++ DataAccessLibrary/UserModels/Aspnetroleclaims.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DataAccessLibrary.UserModels
 {
-    [Table("aspnetroleclaims")]
+    [Table("roleclaims")]
     public partial class Aspnetroleclaims
     {
         [Key]
--- DataAccessLibrary/UserModels/Aspnetusertokens.cs
+++ DataAccessLibrary/UserModels/Aspnetusertokens.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DataAccessLibrary.UserModels
 {
-    [Table("aspnetusertokens")]
+    [Table("usertokens")]
     public partial class Aspnetusertokens
     {
        
